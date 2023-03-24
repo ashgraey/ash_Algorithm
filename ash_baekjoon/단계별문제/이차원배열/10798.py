280f47292a562f71bb8302515edf21cf0efcc195
ashgraey <123621863+ashgraey@users.noreply.github.com>
2023-03-24 15:53:25 +0900

    sd

--- ash_baekjoon/단계별문제/이차원배열/10798.py
+++ ash_baekjoon/단계별문제/이차원배열/10798.py
@@ -11,13 +11,13 @@ fghij
 출력//
 Aa0FfBb1GgCc2HhDd3IiEe4Jj
 '''
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-# words = []
-# for _ in range(5) :
-#     word = list(input())
-#     words.append(word)
+words = []
+for _ in range(5) :
+    word = input()
+    words.append(word)
 
 # words = [
 #     "ABCDE",
@@ -27,21 +27,49 @@ input = sys.stdin.readline
 #     "fghij"
 # ]
 
-words = [
-    "AABCDD",
-    "afzz",
-    "09121",
-    "a8EWg6",
-    "P5h3kx"
-]
+# words = [
+#     "AABCDD",
+#     "afzz",
+#     "09121",
+#     "a8EWg6",
+#     "P5h3kx"
+# ]
 # 길이값 맥스
+max = 0 
+for i in words : 
+    if max < len(i) :
+        max = len(i)
 
-# print(words[0][0])
+# print(max)
+wordList = []
+for word in words :
+    if len(word) < max :
+        size = max - len(word)
+        # print(size)
+        for j in range(size) :
+            word += "*"
+            # print(word)
+        wordList.append(word)
+    elif len(word) >= max :
+        wordList.append(word)
+# print(wordList)
+
 answer = ""
-for i in range(len(words)) :
-    for j in range(len(words[i])) :
-        # print(words[j][i], end = "")
-        answer += words[j][i]
+for i in range(len(wordList[0])) :
+    for j in range(len(wordList)) :
+        if wordList[j][i] != "*" :
+            answer += wordList[j][i]
 
 print(answer)
+
+
+
+# print(words[0][0])
+# answer = ""
+# for i in range(len(words)) :
+#     for j in range(len(words[i])) :
+#         # print(words[j][i], end = "")
+#         answer += words[j][i]
+
+# print(answer)
 
