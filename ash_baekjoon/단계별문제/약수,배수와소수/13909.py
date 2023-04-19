0cbc11660e9244d7cdb66e5bb3fa7267336c79b5
ashgraey <123621863+ashgraey@users.noreply.github.com>
2023-04-19 23:52:53 +0900

    .0

--- ash_baekjoon/단계별문제/약수,배수와소수/13909.py
+++ ash_baekjoon/단계별문제/약수,배수와소수/13909.py
@@ -1,11 +1,32 @@
 '''
 문제 창문 닫기//
 '''
-
-import sys
+# 규칙성을 찾는 문제// 답을 출력하다보면 입력값  n의 제곱근의 정수형 값이 정답이다.
+# 이런 문제도 있구먼..
+import sys,math
 input = sys.stdin.readline
 
 n = int(input())
-window_list = [0] * n
+print(int(math.sqrt(n)))
 
+# 메모리초과// 입력값의 제한이 너무 커서 배열로 받으면 안됨
+# import sys
+# input = sys.stdin.readline
 
+# n = int(input())
+# window_list = [0] * n
+
+# for i in range(n) :
+
+#     for j in range(i, n, i + 1) :
+#         if window_list[j] == 0 :
+#             window_list[j] = 1 
+#         elif window_list[j] == 1 :
+#             window_list[j] = 0 
+
+# cnt = 0 
+# for window in window_list :
+#     if window == 1 :
+#         cnt += 1
+# print(window_list)
+# print(cnt)
