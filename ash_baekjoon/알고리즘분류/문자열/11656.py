0d2d0748cfec7500cec35be36d5bc0aef418c316
ash <123621863+ashgraey@users.noreply.github.com>
2023-05-10 22:14:15 +0900

    1

---
+++ ash_baekjoon/알고리즘분류/문자열/11656.py
@@ -0,0 +1,20 @@
+'''
+문제 접미사 배열//
+
+알고리즘//
+문자열, 구현
+
+키포인트//
+'''
+import sys
+input = sys.stdin.readline
+
+word = input().rstrip()
+
+arr = []
+for i in range(len(word)) :
+    arr.append(word[i:])
+
+arr = sorted(arr)
+for v in arr :
+    print(v)
