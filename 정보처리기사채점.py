24dda762c1f23dc658e9dcf90ff009e73e132392
ashgraey <ashgraey@gmail.com>
2024-01-29 15:33:43 +0900

    240129

--- 정보처리기사채점.py
+++ 정보처리기사채점.py
@@ -5,10 +5,10 @@ result_list = []
 result = 0
 for i in range(len(my)) :
     if my[i] == answer[i] :
-        result_list.append(str(i + 1) + " : O")
+        result_list.append(str(i + 1) + " : O" + " Answer : " + str(answer[i]))
         result += 5
     else :
-        result_list.append(str(i + 1) + " : X")
+        result_list.append(str(i + 1) + " : X" + " Answer : " + str(answer[i]))
 
 # print(result_list)
 for i in result_list :
